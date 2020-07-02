28da1de07e70ebc9fd2948daab23564c27de3b58
Kushal Shekhar Pradhan <kushal.shekhar@hotmail.com>
2020-07-02 10:17:11 +0530

    Updates

---
+++ precache-manifest.73cc55595868dee7351434d588d85641.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf36e2cc7b9bba3ecdf248739b4c434c",
+    "url": "/React-test/index.html"
+  },
+  {
+    "revision": "76c3a9d5ef2a565547cc",
+    "url": "/React-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34692961fd78f0722187",
+    "url": "/React-test/static/js/2.9c41e083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-test/static/js/2.9c41e083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c3a9d5ef2a565547cc",
+    "url": "/React-test/static/js/main.dd4a7028.chunk.js"
+  },
+  {
+    "revision": "7f1a0db90d8ff9d0806c",
+    "url": "/React-test/static/js/runtime-main.36a1b58d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
